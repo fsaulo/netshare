e3cc876e253a781ae8afa396ea40b2ad5b416c11
Saulo G. Felix <meetnapoleon@protonmail.ch>
2019-10-08 01:51:41 -0300

    Add Logger & Handler modules

---
+++ src/com/util/LoggerHandler.java
@@ -0,0 +1,28 @@
+package com.util;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class LoggerHandler {
+  private static Logger loggerObject = null;
+  private String referenceToClass;
+
+  public LoggerHandler(String referenceToClass) {
+    this.referenceToClass = referenceToClass;
+  }
+
+  public Logger getGenericConsoleLogger() {
+    LogManager.getLogManager().reset();
+    LoggerFormatter format = new LoggerFormatter();
+    ConsoleHandler console = new ConsoleHandler();
+
+    loggerObject = Logger.getLogger(referenceToClass);
+
+    console.setLevel(Level.CONFIG);
+    console.setFormatter(format);
+    loggerObject.addHandler(console);
+    return loggerObject;
+  }
+}
