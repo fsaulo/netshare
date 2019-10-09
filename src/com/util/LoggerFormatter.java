78096b16234a9837284c598789be97b6b93cbc1a
Saulo G. Felix <meetnapoleon@protonmail.ch>
2019-10-09 20:50:06 -0300

    Fix issues in LoggerHandler module

--- src/com/util/LoggerFormatter.java
+++ src/com/util/LoggerFormatter.java
@@ -1,7 +1,7 @@
 package com.util;
 
 import java.text.DateFormat;
-import java.SimpleDateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
@@ -11,12 +11,12 @@ public class LoggerFormatter extends Formatter {
   private static final DateFormat dataStyle =
     new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
 
-    public String setLogsFormat(LogRecord record) {
+    public String format(LogRecord record) {
       StringBuilder builder = new StringBuilder(1000);
       builder.append(record.getLevel()).append(": ");
       builder.append("[").append(record.getSourceClassName()).append(".");
       builder.append(record.getSourceMethodName()).append("]. ");
-      builder.append(dataStyle.format(new Data(record.getMillis()))).append(": ");
+      builder.append(dataStyle.format(new Date(record.getMillis()))).append(": ");
       builder.append(formatMessage(record));
       builder.append("\n");
       return builder.toString();
